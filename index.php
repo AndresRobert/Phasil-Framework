5b7e53b0d49f79fc593e8fd03d70557c02fe6899
Andres Robert <andresrobert@icloud.com>
2020-07-04 22:00:56 -0400

    landing page

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-Hello
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phasil Framework</title>
+    <link href="//simple.acode.cl/src/acode.min.css" rel="stylesheet">
+</head>
+<body>
+<h1>Phasil</h1>
+<p>PHP API Simple Layout</p>
+<script src="//simple.acode.cl/src/acode.min.js"></script>
+<script src="https://kit.fontawesome.com/a93fffc8fc.js" crossorigin="anonymous"></script>
+</body>
+</html>
